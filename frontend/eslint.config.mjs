b2694dff27d6b3ba0ff4340a08dbd71d31a5e698
zeynep dursun <zeynepkdursun@gmail.com>
2026-05-04 20:53:56 +0300

    Cleaned up Next.js files for Flutter migration

--- frontend/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import { FlatCompat } from '@eslint/eslintrc'
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname
-})
-
-export default [...compat.extends('next/core-web-vitals')]
-
